da2e59e9c601f6541c010bb3126f03f70c187764
Vinicius <vinicius@distribuidoracuiaba.com>
2020-06-14 14:54:36 -0400

    Criado atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,17 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=35):
         self.idade = idade
         self.nome = nome
         self.filhos = list(filhos)
 
+
     def cumprimentar(self):
         return f'Olá {id(self)} '
 
-
-
 if __name__ == '__main__':
-    nicio = Pessoa(nome='Nicio')
-    heloise = Pessoa(nicio, nome='Heloise')
+    nicio = Pessoa(nome='Nicio', idade='35')
+    heloise = Pessoa(nicio, nome='Heloise', idade='29')
     print(Pessoa.cumprimentar(nicio))
     print(id(nicio))
     print(nicio.cumprimentar())
@@ -19,4 +19,8 @@ if __name__ == '__main__':
     print(nicio.idade)
     for filho in heloise.filhos:
         print(filho.nome)
-
+    nicio.olhos = 3
+    print(heloise.__dict__)
+    print(nicio.__dict__)
+    print(nicio.olhos)
+    print(heloise.olhos)
